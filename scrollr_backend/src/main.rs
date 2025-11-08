2565f8644badb62ffd5f65c75896e10855f27e02
Enanimate <spa0703@gmail.com>
2025-11-08 08:05:20 -0500

    Added a logging feature using the log crate which will asynchronously write our logs to a given path. Additionally, these write operations are performed concurrent to normal operations so logging should never make a significant difference to the time at which the program can accomplish normal tasks.

--- scrollr_backend/src/main.rs
+++ scrollr_backend/src/main.rs
@@ -1,12 +1,20 @@
+use std::path::PathBuf;
+
 use finance_service::start_finance_services;
 use futures_util::future::join_all;
 use dotenv::dotenv;
+use utils::{info, init_async_logger};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
     let mut handles = Vec::new();
+
+    match init_async_logger(PathBuf::from("websocket.log")) {
+        Ok(_) => info!("Async logging initialized successfully"),
+        Err(e) => eprintln!("Failed to set logger: {}", e)
+    }
 
     handles.push(tokio::spawn(start_finance_services()));
 
